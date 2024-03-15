92ae7e56fde5acb557914d251b328029fbd3d8d7
Kostiantyn Kryvorotov <fatuglyaces@gmail.com>
2024-03-15 21:54:43 +0100

    15/03 21:54

--- HomeTasks/Verstka/07.03/script.js
+++ HomeTasks/Verstka/07.03/script.js
@@ -120,35 +120,57 @@ function changeActiveArrow() {}
 
 changeActiveBtn();
 
+// CLIENTS section
+
 const clients = [
-	'./media/microsoft 2.png',
+	'./media/microsoft.png',
 	'./media/amazon.png',
 	'./media/google.png',
 	'./media/wallmart.png',
+	'./media/antivir.png',
+	'./media/carecredit.png',
+	'./media/disney.png',
+	'./media/hyatt.png',
+	'./media/hyundai.png',
+	'./media/redbull.png',
+	'./media/stihl.png',
+	'./media/thermaltake.png',
+	'./media/truman-state-university.png',
+	'./media/vichy.png',
+	'./media/weiler.png',
+	'./media/bolt.png',
 ];
 
 const clients_content = document.querySelector('.clients_content');
 const clients_slider = document.createElement('div');
 clients_slider.className = 'clients_slider';
 
-for (let i = 0; i < clients.length; i += 4) {
-	const slide_group = document.createElement('div');
-	slide_group.className = 'slide_group';
-	for (let j = 0; j < 4; j++) {
-		const client_slide = document.createElement('img');
-		client_slide.src = clients[i];
-		client_slide.alt = 'client';
-		client_slide.className = 'client_slide';
-		slide_group.append(client_slide);
+for (let i = 0; i < clients.length; i++) {
+	const client_slide = document.createElement('img');
+	client_slide.src = clients[i];
+	client_slide.alt = 'client';
+	client_slide.className = 'client_slide';
+
+	clients_slider.append(client_slide);
+}
+clients_content.append(clients_slider);
+
+// SLIDER
+
+const allSlides = document.querySelectorAll('.client_slide');
+for (let i = 0; i < allSlides.length; i++) {
+	if (i < 4) {
+		allSlides[i].style.display = 'block';
+	} else {
+		allSlides[i].style.display = 'none';
 	}
-	clients_slider.append(slide_group);
-	clients_content.append(clients_slider);
 }
 
-// __________________________________
-
+// BUTTONS
+const clients_bottom_content = document.createElement('div');
+clients_bottom_content.className = 'clients_bottom_content content_line';
 const clients_triggers = document.createElement('div');
-clients_triggers.className = 'clients_triggers';
+clients_triggers.className = 'clients_triggers content_line';
 
 const clients_leftBtn = document.createElement('div');
 clients_leftBtn.className = 'clients_left_button';
@@ -164,8 +186,6 @@ clients_rightArrow.src = './media/right.png';
 clients_rightArrow.alt = 'right arrow';
 clients_rightBtn.append(clients_rightArrow);
 
-clients_triggers.append(clients_leftBtn, clients_rightBtn);
-
 const clients_slider_btns = document.createElement('div');
 clients_slider_btns.className = 'clients_slider_btns';
 
@@ -173,53 +193,66 @@ for (let i = 0; i < clients.length; i++) {
 	const clients_round_btn = document.createElement('button');
 	clients_round_btn.className = 'clients_round_btn';
 	clients_slider_btns.append(clients_round_btn);
+	if (i >= 4) {
+		clients_round_btn.style.display = 'none';
+	}
 }
 
-clients_slider.append(clients_slider_btns, clients_triggers);
-clients_content.append(clients_slider);
+clients_triggers.append(clients_leftBtn, clients_rightBtn);
+clients_bottom_content.append(clients_slider_btns, clients_triggers);
+clients_content.append(clients_slider, clients_bottom_content);
 
-rightBtn.onclick = () => {
-	h2_item.style.opacity = 0;
-	setTimeout(() => {
-		if (h2_index !== header_h.length - 1) {
-			h2_index++;
-		} else {
-			h2_index = 0;
-		}
-		h2_item.innerHTML = header_h[h2_index];
-		h2_item.style.opacity = 1;
-		rightBtn.classList.add('active');
-		leftBtn.classList.remove('active');
-		changeActiveBtn();
-	}, 500);
+let slider_index = 0;
+const card_width = 1110;
+
+console.log('Slider index beforeclick:', slider_index);
+
+clients_rightBtn.onclick = () => {
+	if (slider_index + 4 < clients.length) {
+		slider_index += 4;
+	} else {
+		slider_index = 0;
+	}
+	console.log('Slider index click:', slider_index);
+	clients_rightBtn.classList.add('active');
+	clients_leftBtn.classList.remove('active');
+	updateSlider();
+	changeActiveClientBtn();
 };
 
-leftBtn.onclick = () => {
-	h2_item.style.opacity = 0;
-	setTimeout(() => {
-		if (h2_index !== 0) {
-			h2_index--;
-		} else {
-			h2_index = header_h.length - 1;
-		}
-		h2_item.innerHTML = header_h[h2_index];
-		h2_item.style.opacity = 1;
-		leftBtn.classList.add('active');
-		rightBtn.classList.remove('active');
-		changeActiveBtn();
-	}, 500);
+clients_leftBtn.onclick = () => {
+	if (slider_index - 4 >= 0) {
+		slider_index -= 4;
+	} else {
+		slider_index = clients.length - 4;
+	}
+	console.log('Slider index after:', slider_index);
+	clients_leftBtn.classList.add('active');
+	clients_rightBtn.classList.remove('active');
+	updateSlider();
+	changeActiveClientBtn();
 };
 
-function changeActiveBtn() {
-	const allBtns = document.querySelectorAll('.round_btn');
+function updateSlider() {
+	for (let i = 0; i < allSlides.length; i++) {
+		if (i >= slider_index && i < slider_index + 4) {
+			allSlides[i].style.display = 'block';
+		} else {
+			allSlides[i].style.display = 'none';
+		}
+	}
+}
+
+function changeActiveClientBtn() {
+	const allBtns = document.querySelectorAll('.clients_round_btn');
+	const activeIndex = Math.floor(slider_index / 4);
 	allBtns.forEach((elem, index) => {
-		if (h2_index === index) {
+		if (activeIndex === index) {
 			elem.classList.add('active');
 		} else {
 			elem.classList.remove('active');
 		}
 	});
 }
-function changeActiveArrow() {}
 
-changeActiveBtn();
+changeActiveClientBtn();
